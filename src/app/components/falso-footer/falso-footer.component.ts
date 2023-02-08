6f4fa812ee97e62426b2ef7afd10752f87269771
cedai2020 <67275960+cedai2020@users.noreply.github.com>
2023-02-08 12:45:42 -0600

    Funcionalidad boton back

--- src/app/components/falso-footer/falso-footer.component.ts
+++ src/app/components/falso-footer/falso-footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
+import { Location } from "@angular/common";
 
 @Component({
   selector: 'app-falso-footer',
@@ -8,12 +9,16 @@ import { Router } from "@angular/router";
 })
 export class FalsoFooterComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private location: Location) { }
 
   ngOnInit() {}
 
   irHome() {
     this.router.navigate(['/home'])
+  }
+
+  irBack() {
+    this.location.back()
   }
 
 }
